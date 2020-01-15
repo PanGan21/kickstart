0a8b153c5631a5a4119c105bd0f22d10573eb3a8
Panagiotis Ganelis <panagiotis.ganelis-cic.netherlands@ibm.com>
2020-01-15 23:19:38 +0100

    add layout

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,5 +1,16 @@
 import Web3 from "web3";
 
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+if (typeof web3 != "undefined" && typeof window.web3 !== "undefined") {
+  // We are in the browser and metamask is running
+  web3 = new Web3(window.web3.currentProvider);
+} else {
+  // we are on the server or the user is not running metamask
+  const provider = new Web3.providers.HttpProvider(
+    "https://rinkeby.infura.io/v3/271ef2b747b2493c9150c821c5ecd307"
+  );
+  web3 = new Web3(provider);
+}
 
 export default web3;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import factory from "../ethereum/factory";
+import { Card, Button } from "semantic-ui-react";
+import Layout from "../components/Layout";
 
-export default () => {
-  return <h1>This is the new campaign list page!!!</h1>;
+const CampaignIndex = props => {
+  const renderCampaign = () => {
+    const items = props.campaigns.map(address => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true
+      };
+    });
+    return <Card.Group items={items} />;
+  };
+
+  return (
+    <Layout>
+      <div>
+        <link
+          rel="stylesheet"
+          href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
+        />
+        <h3>Open Campaigns</h3>
+        {renderCampaign()}
+        <Button content="Create Campaign" icon="add circle" primary />
+      </div>
+    </Layout>
+  );
 };
+
+CampaignIndex.getInitialProps = async () => {
+  const campaigns = await factory.methods.getDeployedCampains().call();
+  return { campaigns };
+};
+
+export default CampaignIndex;
