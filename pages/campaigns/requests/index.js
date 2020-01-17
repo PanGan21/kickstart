cfed3803f6425e25a63f10116a8176400bd55403
Panagiotis Ganelis <panagiotis.ganelis-cic.netherlands@ibm.com>
2020-01-17 16:12:04 +0100

    add the request list component

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,24 +1,70 @@
 import React from "react";
-import { Button } from "semantic-ui-react";
-import Layout from "../../../components/Layout";
+import { Button, Table } from "semantic-ui-react";
 import { Link } from "../../../routes";
+import Layout from "../../../components/Layout";
+import Campaign from "../../../ethereum/campaign";
+import RequestRow from "../../../components/RequestRow";
 
 const RequestIndex = props => {
+  // Destructure Table
+  const { Header, Row, HeaderCell, Body } = Table;
+
+  const renderRows = () => {
+    return props.requests.map((request, index) => {
+      return (
+        <RequestRow
+          key={index}
+          id={index}
+          request={request}
+          address={props.address}
+          approversCount={props.approversCount}
+        />
+      );
+    });
+  };
   return (
     <Layout>
       <h3>Requests</h3>
       <Link route={`/campaigns/${props.address}/requests/new`}>
         <a>
-          <Button primary>Add Requests</Button>
+          <Button primary floated="right" style={{ marginBottom: 10 }}>
+            Add Requests
+          </Button>
         </a>
       </Link>
+      <Table>
+        <Header>
+          <Row>
+            <HeaderCell>ID</HeaderCell>
+            <HeaderCell>Description</HeaderCell>
+            <HeaderCell>Amount</HeaderCell>
+            <HeaderCell>Recipient</HeaderCell>
+            <HeaderCell>Approval Count</HeaderCell>
+            <HeaderCell>Approve</HeaderCell>
+            <HeaderCell>Finalize</HeaderCell>
+          </Row>
+        </Header>
+        <Body>{renderRows()}</Body>
+      </Table>
+      <div>Found {props.requestCount} requests.</div>
     </Layout>
   );
 };
 
-RequestIndex.getInitialProps = props => {
+RequestIndex.getInitialProps = async props => {
   const { address } = props.query;
-  return { address: address };
+  const campaign = Campaign(address);
+  const requestCount = await campaign.methods.getRequestsCount().call();
+  const approversCount = await campaign.methods.approversCount().call();
+
+  const requests = await Promise.all(
+    Array(parseInt(requestCount))
+      .fill()
+      .map((element, index) => {
+        return campaign.methods.requests(index).call();
+      })
+  );
+  return { address, requests, requestCount, approversCount };
 };
 
 export default RequestIndex;
